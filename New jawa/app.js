d590f4cff51197effe1821d1722689cfb9c34a08
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-01-26 02:36:39 -0800

    Add files via upload

---
+++ New jawa/app.js
@@ -0,0 +1,26 @@
+    // ### chapter no 01 ####
+//*********************************//
+// Question 01
+
+//alert("Hi sir");
+
+// Question 02
+ //alert("Error! please entera valid password");
+
+//Question 03 
+ //alert("wellcome to js land \n happy coding");
+
+//Question 04
+/// alert("wellcome to js land....");
+// alert("happy coding!");
+
+//Question 05
+ //alert("Hello,,, i can run js through my web browser's console");
+
+//Question 06
+// 
+
+//Question 07 
+ //<body>
+ //<script src="app.js"></script>
+//</body>
